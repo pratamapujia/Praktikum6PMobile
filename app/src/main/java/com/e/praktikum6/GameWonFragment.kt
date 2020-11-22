4743126ddc64a273cd2f3f45887278db0eef334e
Pratama Puji <pujiistiani123@gmail.com>
2020-11-22 18:17:28 +0700

    Initial commit

---
+++ app/src/main/java/com/e/praktikum6/GameWonFragment.kt
@@ -0,0 +1,21 @@
+package com.e.praktikum6
+
+import com.e.praktikum6.databinding.FragmentGameWonBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+
+class GameWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_won, container, false)
+        binding.nextMatchButton.setOnClickListener {
+                view: View -> view.findNavController()
+            .navigate(R.id.action_gameWonFragment_to_titleFragment) }
+        return binding.root
+    }
+}
